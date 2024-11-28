8b819e5773b2655a1342f156e831d7d18382ef19
sejinniemma <jsj0471@naver.com>
2024-11-28 15:12:40 +0900

    1128_002
    - add mark down to Posts Page

--- blog-project/next.config.mjs
+++ blog-project/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'], // 여기에 필요한 호스트 추가
+  },
+};
 
 export default nextConfig;
